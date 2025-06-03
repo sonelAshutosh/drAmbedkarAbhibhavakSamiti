4ec66692e3a9dbf4853fe864b472522d8cb2b8d8
Ashutosh Sonel <ashutoshhoonmain@gmail.com>
2025-06-03 15:36:51 +0530

    Updated the admin root page.

--- src/app/admin/page.js
+++ src/app/admin/page.js
@@ -1,7 +1,28 @@
+import { ArrowLeftSquareIcon, ChartLine } from 'lucide-react'
 import React from 'react'
 
 function AdminPage() {
-  return <div>AdminPage</div>
+  return (
+    <div className="py-4 px-4 lg:px-20">
+      <div className="text-lg tracking-wider font-semibold text-secondary-dark">
+        Admin Dashboard
+      </div>
+
+      <div className="bg-red-200 py-8 my-4 rounded-lg">
+        <div className="flex gap-2">
+          <ArrowLeftSquareIcon />
+          <span>Navigate Using the navbar</span>
+        </div>
+      </div>
+      <div className="bg-gray-100 shadow-lg py-8 my-4 rounded-lg">
+        <div className="flex gap-2 items-baseline">
+          <ChartLine />
+          <span>Analytics Dashboard </span>
+          <span className="text-xs text-gray-500">(Coming Soon ...)</span>
+        </div>
+      </div>
+    </div>
+  )
 }
 
 export default AdminPage
