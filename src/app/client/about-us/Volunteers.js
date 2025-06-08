9d6b6b36e191a67f9eb939f3f40361396ece4e1e
Ashutosh Sonel <ashutoshhoonmain@gmail.com>
2025-06-08 11:34:59 +0530

    Updated volunteer ui in about us

--- src/app/client/about-us/Volunteers.js
+++ src/app/client/about-us/Volunteers.js
@@ -9,7 +9,7 @@ import React, { useContext, useEffect, useState } from 'react'
 
 function VolunteerCard({ volunteer }) {
   return (
-    <div className="aspect-[1/1] w-full p-0.5 bg-gradient-to-tr from-primary-dark to-accent-base rounded-lg group hover:scale-105 transition-all ease-in-out">
+    <div className="aspect-[1/1] max-w-md p-0.5 bg-gradient-to-tr from-primary-dark to-accent-base rounded-lg group hover:scale-105 transition-all ease-in-out">
       <div className="h-full w-full text-secondary-dark dark:text-primary-base bg-primary-base dark:bg-secondary-dark rounded-lg flex flex-col relative transition-all ease-in-out p-1">
         <div className="h-full w-full relative rounded-lg overflow-hidden">
           <Image
